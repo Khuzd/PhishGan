d3363fa89be7efc17c6b700550983c5d3e6739df
Pierrick <pierrick.robic--butez@insa-cvl.fr>
2019-07-15 14:55:27 +0200

    class Normalizer

---
+++ Normalizer.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy import sparse
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing.data import _handle_zeros_in_scale
+from sklearn.utils import check_array
+from sklearn.utils.extmath import row_norms
+from sklearn.utils.sparsefuncs import (min_max_axis)
+from sklearn.utils.sparsefuncs_fast import (inplace_csr_row_normalize_l1, inplace_csr_row_normalize_l2)
+from sklearn.utils.validation import FLOAT_DTYPES
+
+
+class Normalizer(BaseEstimator, TransformerMixin):
+    def __init__(self, norm='l2', axis=1, copy=True):
+        self.norm = norm
+        self.axis = axis
+        self.copy = copy
+        self.norms = None
+        self.sparse_format = None
+
+    def fit(self, X):
+        if self.norm not in ('l1', 'l2', 'max'):
+            raise ValueError("'%s' is not a supported norm" % self.norm)
+
+        if self.axis == 0:
+            self.sparse_format = 'csc'
+        elif self.axis == 1:
+            self.sparse_format = 'csr'
+        else:
+            raise ValueError("'%d' is not a supported axis" % self.axis)
+
+        X = check_array(X, self.sparse_format, copy=self.copy,
+                        estimator='the normalize function', dtype=FLOAT_DTYPES)
+        if self.axis == 0:
+            X = X.T
+
+        if sparse.issparse(X):
+            if self.norm == 'l1':
+                inplace_csr_row_normalize_l1(X)
+            elif self.norm == 'l2':
+                inplace_csr_row_normalize_l2(X)
+            elif self.norm == 'max':
+                _, self.norms = min_max_axis(X, 1)
+        else:
+            if self.norm == 'l1':
+                self.norms = np.abs(X).sum(axis=1)
+            elif self.norm == 'l2':
+                self.norms = row_norms(X)
+            elif self.norm == 'max':
+                self.norms = np.max(X, axis=1)
+            self.norms = _handle_zeros_in_scale(self.norms, copy=False)
+
+    def transform(self, X):
+        X = check_array(X, self.sparse_format, copy=self.copy,
+                        estimator='the normalize function', dtype=FLOAT_DTYPES)
+        if self.axis == 0:
+            X = X.T
+        if sparse.issparse(X):
+            if self.norm == 'l1':
+                inplace_csr_row_normalize_l1(X)
+            elif self.norm == 'l2':
+                inplace_csr_row_normalize_l2(X)
+            elif self.norm == 'max':
+                norms_elementwise = self.norms.repeat(np.diff(X.indptr))
+                mask = norms_elementwise != 0
+                X.data[mask] /= norms_elementwise[mask]
+        else:
+            X /= self.norms[:, np.newaxis]
+
+        if self.axis == 0:
+            X = X.T
+
+        return X
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self.fit(X)
+        return self.transform(X)
